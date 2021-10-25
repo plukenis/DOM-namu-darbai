50e330cf404401078ca5b4ff4bdd83667c12f244
plukenis <ieva.zaboole@gmail.com>
2021-10-25 15:18:18 +0300

    3

--- app.js
+++ app.js
@@ -39,10 +39,7 @@ document
   .querySelectorAll(".new")
   .forEach((e) => (e.style.textDecoration = "underline"));
 //     f. Suskaičiuoti kiek yra gyvūnų kategorijų ir žirafų (jos yra ul tagai);
-console.log(
-  "kiek gyvunu kategoriju: ",
-  document.querySelectorAll("ul").length
-);
+console.log("kiek gyvunu kategoriju: ", document.querySelectorAll("ul").length);
 const zirafos = [];
 document.querySelectorAll("li").forEach((e) => {
   if (e.textContent.search("Žirafa") >= 0) zirafos.push(e.textContent);
@@ -51,27 +48,75 @@ console.log("kiek zirafu: ", zirafos.length);
 console.log("Kokios zirafos:");
 console.log(zirafos);
 //     g. Tagus ul apibraukite rėmeliais ir uždėkite 15px paddingą viršuje ir apačioje ir 50px paddingą kairėje ir dešinėje;
-document.querySelectorAll('ul').forEach(e => {
-    e.style.border = '1px solid red';
-    e.style.padding = '15px 50px';
-})
+document.querySelectorAll("ul").forEach((e) => {
+  e.style.border = "1px solid red";
+  e.style.padding = "15px 50px";
+});
 //     h. Suskaičiuoti kiek yra naujų gyvūnų (su klase new);
-console.log('kiek nauju gyvenu: ', document.querySelectorAll('.new').length);
+console.log("kiek nauju gyvenu: ", document.querySelectorAll(".new").length);
 //     i. Suskaičiuoti atskirai kiek yra naujų gyvūnų kiekvienoje kategorijoje;
+console.log("kiek nauju kievienoje kategorijoje: ");
+document.querySelectorAll("ul").forEach((e) => {
+  const m = [];
+  console.log(e.id.toUpperCase());
 
+  e.querySelectorAll(".new").forEach((el) => {
+    m.push(el.textContent);
+    console.log(el.textContent);
+  });
+  console.log("viso: ", m.length);
+});
 // 3. Elementų events
 //     a. Padaryti tai ką liepia mygtukai Header1 sekcijoje;
-
+document.getElementById("h1-color").addEventListener("click", () => {
+  document.querySelector("h1").style.color = "lightgreen";
+});
+document.getElementById("h1-font").addEventListener("click", () => {
+  document.querySelector("h1").style.fontSize = "10px";
+});
 //     b. Padaryti, kad paspaudus ant i tago jis pakeistų fonto svorį į bold;
-
+const i = document.querySelector("i");
+i.addEventListener("click", () => (i.style.fontWeight = "bold"));
 //     c. Padaryti, kad paspaudus ant tago su klase prices, backgroundas pasikeistų į pilką, o paspaudus dar kartą vėl grįžtu į baltą spalvą;
+let prices = document.querySelector(".prices");
+
+let count = 0;
+
+prices.addEventListener("click", function () {
+  count++;
+  if (count % 2 === 1) {
+    prices.style.backgroundColor = "grey";
+  } else {
+    prices.style.backgroundColor = "white";
+  }
+});
 
 //     d. Padaryti, kad paspaudus ant tago su id contacts, tam tagui būtų pridėta css savybė color = orange;
+let contacts = document.querySelector("#contacts:not(b)");
+contacts.addEventListener("click", function () {
+  if (colorCount > 0) {
+    colorCount = 0;
+  } else {
+    contacts.style.color = "orange";
+  }
+});
 
 //     e. Padaryti taip, kad paspaudus ant padidinti, esančio tage su id contacts, tagui su id contacts būtų pridėta css savybė fontSize = 20px;
+let bigger = document.querySelector("#contacts > u");
+
+bigger.addEventListener("click", function () {
+  contacts.style.fontSize = "20px";
+});
 
 //     f. Padaryti taip, kad paspaudus ant X, esančio tage su id contacts, pridėtos tage su id contacts savybės būtų panaikintos https://stackoverflow.com/questions/18691655/remove-style-on-element
+let x = document.querySelector("#contacts > b");
+let colorCount = 0;
 
+x.addEventListener("click", function () {
+  colorCount++;
+  contacts.style.color = "black";
+  contacts.style.fontSize = null;
+});
 //     g. Padaryti tai ką liepia mygtukai Header2 sekcijoje;
 
 // 4. Elementų grupių events
